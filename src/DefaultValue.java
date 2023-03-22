1860da7a01761586177ceaf089dc7c7c0e5aa7c3
Kasun <kasunnawodshalind@gmail.com>
2023-03-22 18:50:28 +0530

    constructor_project

---
+++ src/DefaultValue.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class DefaultValue {
+    int x;
+    boolean y;
+    public static void main(String[] args) {
+        DefaultValue df = new DefaultValue();
+        System.out.println(df.x);
+        System.out.println(df.y);
+    }
+}
